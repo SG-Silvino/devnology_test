9edaaa2671b7260c1aad110a25a842c6f2d3fb4e
SG Silvino <sgsilvino@gmail.com>
2022-05-20 18:16:51 -0100

    bottom nav added

--- lib/widgets/bottom_navigator.dart
+++ lib/widgets/bottom_navigator.dart
@@ -1,41 +1,63 @@
+import 'package:devnology_test/config/navigation.dart';
+import 'package:devnology_test/config/theme.dart';
+import 'package:devnology_test/widgets/cart_badge_wdgt.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class MyBottomNav extends StatefulWidget {
-  const MyBottomNav({Key? key}) : super(key: key);
+// ignore: must_be_immutable
+class MyBottomNav extends StatelessWidget {
+  MyBottomNav({Key? key}) : super(key: key);
 
-  @override
-  State<MyBottomNav> createState() => _MyBottomNavState();
-}
+  List navRoutes = [
+    '/home',
+    '/comingSoon',
+    '/cart',
+    '/comingSoon',
+    '/comingSoon',
+  ];
 
-class _MyBottomNavState extends State<MyBottomNav> {
   int _selectedIndex = 0;
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+  String iconPath = "assets/icons/";
 
   @override
   Widget build(BuildContext context) {
+    void _onItemTapped(int index) {
+      _selectedIndex = index;
+
+      Nav.goAndReplacePageNamed(context, navRoutes[index]);
+    }
+
     return BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: AppTheme.primaryColor,
+      unselectedItemColor: AppTheme.letterWhite,
+      iconSize: 18,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(iconPath + 'home.svg'),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(iconPath + 'search.svg'),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: CartIconBadge(-8),
+          label: 'Cart',
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(iconPath + 'person.svg'),
+          label: 'Profile',
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(iconPath + 'more.svg'),
+          label: 'More',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: AppTheme.secondaryColor,
+      onTap: _onItemTapped,
     );
   }
 }
