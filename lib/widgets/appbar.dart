fa5b7ac9cddefe5bd7561b37a95685e3a239e8f4
SG Silvino <sgsilvino@gmail.com>
2022-05-20 18:17:02 -0100

    appbar added

---
+++ lib/widgets/appbar.dart
@@ -0,0 +1,24 @@
+import 'package:devnology_test/config/navigation.dart';
+import 'package:devnology_test/config/theme.dart';
+import 'package:flutter/material.dart';
+
+myAppBar(BuildContext context, {List<Widget>? actions}) {
+  bool canPop = Navigator.canPop(context);
+
+  return AppBar(
+    backgroundColor: AppTheme.primaryColor,
+    title: Image.asset(
+      'assets/images/Logo.png',
+      width: 170,
+      height: 28,
+    ),
+    centerTitle: canPop ? true : false,
+    leading: canPop
+        ? IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () => Nav.goBack(context),
+          )
+        : null,
+    actions: actions,
+  );
+}
