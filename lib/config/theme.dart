8688335f9a675d26513a4dcaca181c8b251afaf9
SG Silvino <sgsilvino@gmail.com>
2022-05-20 18:20:37 -0100

    config theme

---
+++ lib/config/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static Color primaryColor = const Color(0xff2E3746);
+  static Color secondaryColor = const Color(0xffFFC600);
+
+  static Color bgLight = Colors.white;
+
+  static Color letterWhite = Colors.white;
+  static Color titleColor = const Color(0xff2E3746);
+  static Color subTitleColor = Colors.black;
+
+  static Color iconLight = Colors.white;
+}
