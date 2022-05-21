356a1c433ee4044b6144650603ab052a57775ba2
SG Silvino <sgsilvino@gmail.com>
2022-05-21 10:39:29 -0100

    TextStyle

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -11,4 +11,21 @@ class AppTheme {
   static Color subTitleColor = Colors.black;
 
   static Color iconLight = Colors.white;
+
+  static TextStyle textStyleH1 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w700,
+    color: primaryColor,
+  );
+
+  static TextStyle textStyleParagraph({
+    double fontSize = 14,
+    FontWeight fontWeight = FontWeight.w400,
+    Color? color,
+  }) =>
+      TextStyle(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        color: color ?? primaryColor,
+      );
 }
