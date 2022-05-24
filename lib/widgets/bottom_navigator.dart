a1ff30dc83dc4d0db1fe878739afbadbcad955c1
SG Silvino <sgsilvino@gmail.com>
2022-05-24 19:15:05 -0100

    navBar ready

--- lib/widgets/bottom_navigator.dart
+++ lib/widgets/bottom_navigator.dart
@@ -4,10 +4,17 @@ import 'package:devnology_test/widgets/cart_badge_wdgt.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-// ignore: must_be_immutable
-class MyBottomNav extends StatelessWidget {
-  MyBottomNav({Key? key}) : super(key: key);
+int _selectedIndex = 0;
 
+// ignore: must_be_immutable
+class MyBottomNav extends StatefulWidget {
+  const MyBottomNav({Key? key}) : super(key: key);
+
+  @override
+  State<MyBottomNav> createState() => _MyBottomNavState();
+}
+
+class _MyBottomNavState extends State<MyBottomNav> {
   List navRoutes = [
     '/home',
     '/comingSoon',
@@ -16,48 +23,66 @@ class MyBottomNav extends StatelessWidget {
     '/comingSoon',
   ];
 
-  int _selectedIndex = 0;
-
   String iconPath = "assets/icons/";
 
   @override
   Widget build(BuildContext context) {
     void _onItemTapped(int index) {
-      _selectedIndex = index;
+      setState(() {
+        _selectedIndex = index;
+      });
 
-      Nav.goAndReplacePageNamed(context, navRoutes[index]);
+      Nav.goAndReplacePageNamed(navRoutes[index]);
+    }
+
+    Color _itemIsSelected(int index) {
+      if (index == _selectedIndex) {
+        return AppTheme.secondaryColor;
+      } else {
+        return AppTheme.iconLight;
+      }
     }
 
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
       backgroundColor: AppTheme.primaryColor,
       unselectedItemColor: AppTheme.letterWhite,
+      selectedItemColor: AppTheme.secondaryColor,
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
       iconSize: 18,
       items: <BottomNavigationBarItem>[
         BottomNavigationBarItem(
-          icon: SvgPicture.asset(iconPath + 'home.svg'),
+          icon: SvgPicture.asset(
+            iconPath + 'home.svg',
+            color: _itemIsSelected(0),
+          ),
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: SvgPicture.asset(iconPath + 'search.svg'),
+          icon: SvgPicture.asset(iconPath + 'search.svg',
+              color: _itemIsSelected(1)),
           label: 'Search',
         ),
         BottomNavigationBarItem(
-          icon: CartIconBadge(-8),
+          icon: CartIconBadge(-8, color: _itemIsSelected(2)),
           label: 'Cart',
         ),
         BottomNavigationBarItem(
-          icon: SvgPicture.asset(iconPath + 'person.svg'),
+          icon: SvgPicture.asset(
+            iconPath + 'person.svg',
+            color: _itemIsSelected(3),
+          ),
           label: 'Profile',
         ),
         BottomNavigationBarItem(
-          icon: SvgPicture.asset(iconPath + 'more.svg'),
+          icon: SvgPicture.asset(
+            iconPath + 'more.svg',
+            color: _itemIsSelected(4),
+          ),
           label: 'More',
         ),
       ],
-      currentIndex: _selectedIndex,
-      selectedItemColor: AppTheme.secondaryColor,
-      onTap: _onItemTapped,
     );
   }
 }
