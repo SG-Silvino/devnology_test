4d68e8e802fee290bcc29ea52ad1c250d1358384
SG Silvino <sgsilvino@gmail.com>
2022-05-24 19:20:43 -0100

    add to cart

--- lib/model/cart.dart
+++ lib/model/cart.dart
@@ -1,18 +1,22 @@
 import 'package:devnology_test/config/supabase_config.dart';
 import 'package:devnology_test/main.dart';
 import 'package:devnology_test/model/product.dart';
+import 'package:flutter/material.dart';
 
 class MyCart {
+  String? id;
   int? productQtd;
   Product? product;
 
   MyCart({
+    this.id,
     this.product,
     this.productQtd,
   });
 
   factory MyCart.fromMap(Map<String, dynamic> map) {
     return MyCart(
+      id: map['id'],
       product: Product.fromMap(map['product_id']),
       productQtd: map['product_qtd'],
     );
@@ -21,10 +25,13 @@ class MyCart {
   Future getCarts() async {
     await supabase
         .from('Cart')
-        .select('''product_qtd, product_id (*)''')
+        .select('''id, product_qtd, product_id (*)''')
+        .eq('user_id', userID)
+        .order('last_update', ascending: false)
         .execute()
         .then((value) {
           cartList = value.data as List;
+          cartListPriceTotal = 0;
 
           for (var product in cartList) {
             MyCart cart = MyCart.fromMap(product);
@@ -32,10 +39,46 @@ class MyCart {
             double price =
                 double.parse(cart.product!.price!.toStringAsFixed(2));
 
-            cartListPriceTotal += (price * cart.productQtd!);
+            cartListPriceTotal += price * cart.productQtd!;
           }
         });
 
-    cartBadge = cartList.length;
+    cartBadge.value = cartList.length;
+  }
+
+  Future addToCart(BuildContext context, Product product) async {
+    Future addNewProductToCart() async {
+      await supabase
+          .from("Cart")
+          .insert({
+            "user_id": userID,
+            "product_id": product.id,
+          })
+          .execute()
+          .then((value) {
+            getCarts();
+          });
+    }
+
+    if (cartList.isNotEmpty) {
+      for (var i = 0; i < cartList.length; i++) {
+        MyCart cart = MyCart.fromMap(cartList[i]);
+
+        if (cart.product!.id == product.id) {
+          int qtd = cart.productQtd! + 1;
+
+          await supabase
+              .from("Cart")
+              .update({"product_qtd": qtd})
+              .eq('id', cart.id)
+              .execute()
+              .then((value) => getCarts());
+        } else {
+          await addNewProductToCart();
+        }
+      }
+    } else {
+      await addNewProductToCart();
+    }
   }
 }
