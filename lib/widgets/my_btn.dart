be6e116e96853ef80c12a294cdac55c1b9050b8c
SG Silvino <sgsilvino@gmail.com>
2022-05-26 16:11:02 -0100

    btn size responsive

--- lib/widgets/my_btn.dart
+++ lib/widgets/my_btn.dart
@@ -1,5 +1,6 @@
 import 'package:devnology_test/config/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MyBtn {
   static roundedBtnWithIcon({
@@ -11,12 +12,12 @@ class MyBtn {
     required void Function()? onPressed,
   }) {
     return SizedBox(
-      width: 140,
-      height: 40,
+      width: Get.width > 360 ? 140 : 125,
+      height: customIcon != null ? 38 : 40,
       child: ElevatedButton(
         onPressed: onPressed,
         style: ElevatedButton.styleFrom(
-          padding: const EdgeInsets.only(left: 16, right: 8),
+          padding: const EdgeInsets.only(left: 18, right: 12),
           primary: backgroundColor,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(30),
@@ -29,11 +30,17 @@ class MyBtn {
               "$label".toUpperCase(),
               style: TextStyle(
                 color: foregroundColor ?? AppTheme.letterWhite,
-                fontSize: 12,
+                fontSize: Get.width > 360 ? 12 : 10,
                 fontWeight: FontWeight.w900,
               ),
             ),
-            customIcon ?? Icon(iconData, color: foregroundColor)
+            customIcon != null
+                ? SizedBox(height: Get.width > 360 ? 18 : 16, child: customIcon)
+                : Icon(
+                    iconData,
+                    color: foregroundColor,
+                    size: Get.width > 360 ? 18 : 16,
+                  ),
           ],
         ),
       ),
